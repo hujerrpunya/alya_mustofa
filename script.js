d5771e02fa7cc868da56d86671ef422798323d5a
SMKN 10 BATAM OFFICIAL WEB <pecintadoraemon324@gmail.com>
2024-08-13 07:07:17 +0700

    request

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayFlowers() {
+    const flowerContainer = document.getElementById('flower-container');
+    const flower = document.createElement('div');
+    flower.className = 'flower';
+    flower.innerHTML = '🩰°˖𓍢ִ໋🌷͙֒✧°.🎀༘⋆⋆.˚🦋༘⋆⋆.˚🦋༘⋆🤍🎀🧸🌷🍭⋆｡˚ ☁︎ ˚｡⋆｡˚☽˚｡⋆Alyaa Mustofa˗ˋˏ ♡ ˎˊ˗';
+    flowerContainer.appendChild(flower);
+}
+
+
